7a6e000a15a06ddd90de6c7425d96ae0578c111a
Andres <andres@kemeny.cl>
2020-04-11 00:18:29 -0400

    cajero done

--- cajero/cajero.py
+++ cajero/cajero.py
@@ -11,12 +11,34 @@ Billetes: 20.000, 10.000, 5.000, 2.000, 1.000, 500, 100, 50, 10.
 
 def cajero(pagar, pagado):
   vuelto = pagado - pagar
-  return vuelto
+  ans = []
+  bills = [20000, 10000, 5000, 2000, 1000, 500, 100, 50, 10]
+
+  for i in bills:
+    if (vuelto // i) > 0:
+      ans.extend([i for x in range(vuelto // i)])
+      vuelto -= i * (vuelto // i)
+
+  return ans
 
 def main():
   pagar = int(input("Ingesa cuanto debes pagar: "))
   pagado = int(input("Ingresa cuanto pagaste: "))
 
-  print(cajero(pagar, pagado))
+  vuelto = cajero(pagar, pagado)
+
+  print(
+    "Debe entregar " + str(vuelto.count(20000)) + " Billetes de $20.000 \n" +
+    "Debe entregar " + str(vuelto.count(10000)) + " Billetes de $10.000 \n" +
+    "Debe entregar " + str(vuelto.count(5000)) + " Billetes de $5.000 \n" +
+    "Debe entregar " + str(vuelto.count(2000)) + " Billetes de $2.000 \n" +
+    "Debe entregar " + str(vuelto.count(1000)) + " Billetes de $1.000 \n" +
+    "Debe entregar " + str(vuelto.count(500)) + " Monedas de $500 \n" +
+    "Debe entregar " + str(vuelto.count(100)) + " Monedas de $100 \n" +
+    "Debe entregar " + str(vuelto.count(50)) + " Monedas de $50 \n" +
+    "Debe entregar " + str(vuelto.count(10)) + " Monedas de $10 \n" 
+  )
+
+
 
 main()
