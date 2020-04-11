6c2c807856b5efbf693f2d92dd3d2428fa72bfcd
Andres <andres@kemeny.cl>
2020-04-10 22:18:14 -0400

    comment

--- 4-even-odd/4-even-odd.py
+++ 4-even-odd/4-even-odd.py
@@ -1,3 +1,9 @@
+"""
+Pide al usuario que ingrese un numero.
+El programa debera retornar por pantalla si el numero es par o impar.
+Si el numero es multiplo de 4 debera retornar por pantalla un mensaje diferente
+"""
+
 def check(num):
   if (num % 4) == 0:
     return "Numero multiplo de 4"
